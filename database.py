c37291be3e5806db3a90a57314d223518dd438ae
Magg-droid <csparrrrrow@gmail.com>
2026-06-28 17:42:53 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,28 +1,44 @@
 import sqlite3
+import os
 
-conn = sqlite3.connect("data/staff.db")
+# Create data folder if it doesn't exist
+os.makedirs(
+    "data",
+    exist_ok=True
+)
+
+conn = sqlite3.connect(
+    "data/staff.db"
+)
+
 cursor = conn.cursor()
 
+
+# Staff table
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS staff(
+
     user_id INTEGER PRIMARY KEY,
     username TEXT,
-
-    current_pf INTEGER DEFAULT 0,
-    lifetime_pf INTEGER DEFAULT 0,
-
+    current_pf INTEGER,
+    lifetime_pf INTEGER,
     pf_type TEXT,
     last_active TEXT
+
 )
 """)
 
+
+# Payment history table
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS payments(
+
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     user_id INTEGER,
     username TEXT,
     completed_pf INTEGER,
     payment_date TEXT
+
 )
 """)
 
